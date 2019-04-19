88a838c762561d9612a986275461319dca3e1dee
Jay Tailor <tailorj64@gmail.com>
2019-04-19 16:01:42 +0530

    Implemented GET task method

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ const Todo = connection.define('Todo', {
     name: Sequelize.STRING,
 })
 
+app.get('/notes', (req,res)=> {
+    Todo.findAll()
+    .then((resp) => {
+        res.json(resp).status(200)
+    })
+    .catch((err)=> {
+        res.json({"error": JSON.stringify(err)}).status(400)
+    })
+})
+
 connection.sync({
     logging: console.log,
     force: true
